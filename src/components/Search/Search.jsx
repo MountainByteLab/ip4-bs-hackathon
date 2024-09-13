c4c1fab3c89e4697af44d629239c1d31bc84ea35
MountainByteLab <amaupromo@gmail.com>
2024-09-13 10:22:39 -0400

    Add cursor pointer to buttons and update search styling

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,31 +1,39 @@
-import React from 'react';
-import searchIcon from "../../assets/images/search.svg"; 
+import React from "react";
+import searchIcon from "../../assets/images/search.svg";
 import "./Search.scss";
 
-
-function Search( {searchTerm, setSearchTerm, showsList, setSearchedShow} ) {
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        let foundShow = showsList.find(show=>show.show_name.toLowerCase()===searchTerm.toLowerCase())
-        console.log(foundShow)
-        setSearchedShow(foundShow)
-    }
-
-    return (
-        <div className="search-bar-container">
-            <h2>Step 1: Search Stream what you like</h2>
-            <div className="search-bar">
-                <input 
-                    type="text" 
-                    placeholder="Search" 
-                    value={searchTerm} 
-                    onChange={(e) => setSearchTerm(e.target.value)} 
-                />
-                <img src={searchIcon} alt="Search Icon" onClick={handleSubmit} />
-            </div>
-    </div>
+function Search({ searchTerm, setSearchTerm, showsList, setSearchedShow }) {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let foundShow = showsList.find(
+      (show) => show.show_name.toLowerCase() === searchTerm.toLowerCase()
     );
+    console.log(foundShow);
+    setSearchedShow(foundShow);
+    console.log(event.target)
+    // event.target.reset();
+  };
+
+  return (
+    <div className="search-bar-container">
+      <h2>Step 1: Search for your show</h2>
+      <div className="search-bar">
+        <input
+          className="search-bar__input"
+          type="text"
+          placeholder="Search"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img
+          className="search-bar__button"
+          src={searchIcon}
+          alt="Search Icon"
+          onClick={handleSubmit}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Search;
